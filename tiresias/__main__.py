204e65e0b5383ca2758d6787631aee6678880943
Thijsvanede <t.s.vanede@utwente.nl>
2020-05-13 15:13:02 -0700

    Train and test Tiresias implementation through main

--- tiresias/__main__.py
+++ tiresias/__main__.py
@@ -1,8 +1,12 @@
 import argparse
 from preprocessing import Preprocessor
+from tiresias import Tiresias
 from utils import TextHelpFormatter
 
 if __name__ == "__main__":
+########################################################################
+#                           Parse arguments                            #
+########################################################################
     # Parse arguments
     parser = argparse.ArgumentParser(
         prog        = "tiresias.py",
@@ -11,16 +15,41 @@ if __name__ == "__main__":
     )
 
     # Add arguments
-    parser.add_argument('file', help='file to read as input')
-    parser.add_argument('-m', '--max', type=float, default=float('inf'), help='maximum number of items to read from input')
+    group_input = parser.add_argument_group("Input parameters")
+    group_input.add_argument('file', help='file to read as input')
+    group_input.add_argument('-m', '--max', type=float, default=float('inf'), help='maximum number of items to read from input')
+
+    group_tiresias = parser.add_argument_group("Tiresias parameters")
+    group_tiresias.add_argument('-i', '--input' , default=300, help='input  dimension')
+    group_tiresias.add_argument('-l', '--hidden', default=128, help='hidden dimension')
+    group_tiresias.add_argument('-k', '--k'     , default=10 , help='number of concurrent memory cells')
 
     # Parse arguments
     args = parser.parse_args()
 
+    ########################################################################
+    #                              Load data                               #
+    ########################################################################
     # Initialse preprocessor
     preprocessor = Preprocessor()
     # Load data
-    D = preprocessor.load(args.file, max=args.max)
+    D = preprocessor.load(args.file, max=args.max, min_seq_length=20)
 
-    for k, v in D.items():
-        print(k, v)
+    # TODO proper split
+    X_train = [x[:-1] for x in D.values()]
+    y_train = [x[ -1] for x in D.values()]
+    X_test  = [x[:-1] for x in D.values()]
+    import torch
+    y_test  = torch.as_tensor([x[ -1] for x in D.values()])
+
+    ########################################################################
+    #                               Tiresias                               #
+    ########################################################################
+    tiresias = Tiresias(args.input, args.hidden, args.input, args.k)
+    # Train tiresias
+    tiresias.fit(X_train, y_train, batch_size=128, variable=True)
+    # Predict using tiresias
+    y_pred = tiresias.predict(X_test, variable=True)
+
+    from sklearn.metrics import classification_report
+    print(classification_report(y_test, y_pred, digits=4))
