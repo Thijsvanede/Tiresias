88675c46e46c0007cfe3ec2a01e2ab1c43d2d11a
Thijsvanede <t.s.vanede@utwente.nl>
2020-05-15 11:36:32 -0700

    Test ArrayLSTM implementation

--- tiresias/tiresias.py
+++ tiresias/tiresias.py
@@ -64,8 +64,8 @@ class Tiresias(Module):
 
         # Initialise layers
         # self.lstm    = nn.LSTM(input_size, hidden_size, batch_first=True).to(device)
-        self.lstm    = LSTM(input_size, hidden_size).to(device)
-        # self.lstm    = ArrayLSTM(input_size, hidden_size, k)
+        # self.lstm    = LSTM(input_size, hidden_size).to(device)
+        self.lstm    = ArrayLSTM(input_size, hidden_size, k).to(device)
         # self.lstm    = SoftArrayLSTM(input_size, hidden_size, k)
         # self.lstm    = StochasticArrayLSTM(input_size, hidden_size, k)
         self.linear  = nn.Linear(hidden_size, output_size).to(device)
