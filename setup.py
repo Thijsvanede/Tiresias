8a15d1ca97eb624f1dc7816487dfe83e11d6830b
Thijs van Ede <t.s.vanede@utwente.nl>
2021-07-21 14:45:25 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tiresias",
-    version="0.0.1",
+    version="0.0.2",
     author="Thijs van Ede",
     author_email="t.s.vanede@utwente.nl",
     description="Pytorch implementation of Tiresias: Predicting Security Events Through Deep Learning",
